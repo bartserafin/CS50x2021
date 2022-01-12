f181ff5f3ebf9dcd543d1d03a5f3c4492782fa08
bartserafin <86855505+bartserafin@users.noreply.github.com>
2022-01-13 00:00:45 +0100

    Update application.py

--- week9 - Flask/pset9/finance/application.py
+++ week9 - Flask/pset9/finance/application.py
@@ -2,7 +2,7 @@ import os
 
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
-from flask_session import Session
+from flask_session.__init__ import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
